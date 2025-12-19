8130043892bc83a61ad807638857b9efa5d688ac
gayathri341 <shanthi0912ramesh@gmail.com>
2025-12-19 21:49:30 +0530

    Reacting routing process was done and Dashboard and Login files was updated

---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+
+import { Link } from 'react-router-dom'
+export default function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <Link to="/profile">Go to Profile</Link>
+    </div>
+  )
+}
