83f509ba75f2fab225d19a2acff5c67028a4dacc
gayathri341 <shanthi0912ramesh@gmail.com>
2025-12-20 18:30:26 +0530

    Dashboard was updated

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom'
 export default function Dashboard() {
   return (
     <>
       <h2>Dashboard</h2>
       <p>Welcome 🎉</p>
+      <Link to="/profile">
+        <button>Go to Profile</button>
+      </Link>
     </>
   )
 }
