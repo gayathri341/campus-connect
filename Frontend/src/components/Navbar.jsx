01e76cc172f44347bc35f119ea1e008b5b01389f
gayathri341 <shanthi0912ramesh@gmail.com>
2026-02-06 15:37:38 +0530

    navbar ui is ready and perfect

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import logo from '../assets/newlogo.png'
 import Userd from "../assets/userd.png";
 import Logout from "../assets/logout.png";
 import Ring from "../assets/ringing.png";
+import { MdDashboard, MdMessage, MdPeople, MdFolder, MdNotificationsNone, MdPersonOutline, MdLogout } from "react-icons/md"
 
 export default function Navbar() {
   const navigate = useNavigate()
@@ -22,41 +23,62 @@ export default function Navbar() {
           <span>PlaceMent</span>
         </div>
 
-        <nav className="nav-links">
+        <nav className="nav-link">
           <NavLink to="/dashboard" className="nav-item">
-            Dashboard
+            <span className="nav-link-content">
+              <MdDashboard className="nav-icon" />
+              <span className="nav-text">Dashboard</span>
+            </span>
           </NavLink>
+         
           <NavLink to="/messages" className="nav-item">
-            Messages
+            <span className="nav-link-content">
+              <MdMessage className="nav-icon" />
+              <span className="nav-text">Messages</span>
+            </span>
           </NavLink>
+
+         {/* Connections */}
           <NavLink to="/connections" className="nav-item">
-            Connections
+            <span className="nav-link-content">
+              <MdPeople className="nav-icon" />
+              <span className="nav-text">Connections</span>
+            </span>
           </NavLink>
+
+          {/* Resources */}
           <NavLink to="/resources" className="nav-item">
-            Resources
+            <span className="nav-link-content">
+              <MdFolder className="nav-icon" />
+              <span className="nav-text">Resources</span>
+            </span>
           </NavLink>
         </nav>
       </div>
 
       <div className="nav-right">
-        <button
-          className="icon-btn"
-          onClick={() => navigate('/notifications')}
-        >
-         <img src={Ring}></img>
-        </button>
+  <button
+    className="nav-icon-btn"
+    onClick={() => navigate('/notifications')}
+  >
+    <MdNotificationsNone className="nav-top-icon" />
+  </button>
 
-        <button
-          className="icon-btn"
-          onClick={() => navigate('/profile')}
-        >
-         <img src={Userd}></img>
-        </button>
+  <button
+    className="nav-icon-btn"
+    onClick={() => navigate('/profile')}
+  >
+    <MdPersonOutline className="nav-top-icon" />
+  </button>
 
-        <button className="icon-btn logout" onClick={handleLogout}>
-        <img src={Logout}></img>
-        </button>
-      </div>
+  <button
+    className="nav-icon-btn logout"
+    onClick={handleLogout}
+  >
+    <MdLogout className="nav-top-icon" />
+  </button>
+</div>
+
     </header>
   )
 }
